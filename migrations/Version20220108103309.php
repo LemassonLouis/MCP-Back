e4acc43413363c6ee2788d2d004293986f445361
Kevin-Clement <kevin-clement@live.fr>
2022-01-08 11:48:06 +0100

    Adding ingredients entities & fixtures

---
+++ migrations/Version20220108103309.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220108103309 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE ingredient (id INT AUTO_INCREMENT NOT NULL, ing_name VARCHAR(64) NOT NULL, ing_allergen TINYINT(1) DEFAULT NULL, ing_vege TINYINT(1) DEFAULT NULL, ing_unit VARCHAR(32) DEFAULT NULL, ing_price INT DEFAULT NULL, ing_is_archive TINYINT(1) DEFAULT NULL, ing_date_edit DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE ingredient');
+    }
+}
---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\IngredientRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=IngredientRepository::class)
+ * @ApiResource(
+ *      attributes={
+ *          "order"={"ING_name": "ASC"}
+ *      },
+ *      normalizationContext={"groups"={"read:ingredient"}},
+ * )
+ */
+class Ingredient
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"read:ingredient"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_name;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_allergen;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_vege;
+
+    /**
+     * @ORM\Column(type="string", length=32, nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_unit;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_price;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_isArchive;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     * @Groups({"read:ingredient"})
+     */
+    private $ING_date_edit;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getINGName(): ?string
+    {
+        return $this->ING_name;
+    }
+
+    public function setINGName(string $ING_name): self
+    {
+        $this->ING_name = $ING_name;
+
+        return $this;
+    }
+
+    public function getINGAllergen(): ?bool
+    {
+        return $this->ING_allergen;
+    }
+
+    public function setINGAllergen(?bool $ING_allergen): self
+    {
+        $this->ING_allergen = $ING_allergen;
+
+        return $this;
+    }
+
+    public function getINGVege(): ?bool
+    {
+        return $this->ING_vege;
+    }
+
+    public function setINGVege(?bool $ING_vege): self
+    {
+        $this->ING_vege = $ING_vege;
+
+        return $this;
+    }
+
+    public function getINGUnit(): ?string
+    {
+        return $this->ING_unit;
+    }
+
+    public function setINGUnit(?string $ING_unit): self
+    {
+        $this->ING_unit = $ING_unit;
+
+        return $this;
+    }
+
+    public function getINGPrice(): ?int
+    {
+        return $this->ING_price;
+    }
+
+    public function setINGPrice(?int $ING_price): self
+    {
+        $this->ING_price = $ING_price;
+
+        return $this;
+    }
+
+    public function getINGIsArchive(): ?bool
+    {
+        return $this->ING_isArchive;
+    }
+
+    public function setINGIsArchive(?bool $ING_isArchive): self
+    {
+        $this->ING_isArchive = $ING_isArchive;
+
+        return $this;
+    }
+
+    public function getINGDateEdit(): ?\DateTimeInterface
+    {
+        return $this->ING_date_edit;
+    }
+
+    public function setINGDateEdit(?\DateTimeInterface $ING_date_edit): self
+    {
+        $this->ING_date_edit = $ING_date_edit;
+
+        return $this;
+    }
+}
