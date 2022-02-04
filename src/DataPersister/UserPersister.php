00d4ee057cd0fdb6db3d30d3dba04e70988090b8
Kevin-Clement <kevin-clement@live.fr>
2022-02-04 18:37:44 +0100

    User data persister

---
+++ src/DataPersister/UserPersister.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataPersister;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+
+class UserPersister implements DataPersisterInterface
+{
+
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
+
+    public function persist($data)
+    {
+        $data->setIsArchive(true);
+        $this->em->persist($data);
+        $this->em->flush();
+    }
+
+    public function remove($data)
+    {
+        $this->em->remove($data);
+        $this->em->flush();
+    }
+}
